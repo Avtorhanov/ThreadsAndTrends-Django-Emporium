cee443b50f21c6def0a08e26b5ba39bc64cf9fd5
Ahmad Avtorkhanov <example0612@mail.ru>
2024-02-03 14:35:54 +0300

    improved control panel: visibility of orders, user baskets, delivery details.

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,45 @@
-# store/admin.py
 from django.contrib import admin
-from .models import Category, SubCategory, Product, CartItem, Cart
-
-admin.site.register(Category)
-admin.site.register(SubCategory)
-admin.site.register(Product)
-admin.site.register(Cart)
+from .models import Category, SubCategory, Product, Cart, Order, OrderItem, CartItem
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
+    search_fields = ['name']
+    list_filter = ['category']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'subcategory', 'price']
+    search_fields = ['name']
+    list_filter = ['category', 'subcategory']
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['owner']
+    search_fields = ['owner__username']
+    inlines = [CartItemInline]
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['owner', 'total_price', 'is_ordered', 'address', 'phone_number', 'full_name']
+    search_fields = ['owner__username']
+    inlines = [OrderItemInline]
+    fields = ['owner', 'total_price', 'is_ordered', 'address', 'phone_number', 'full_name']
+
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
--- store/models.py
+++ store/models.py
@@ -55,6 +55,9 @@ class Order(models.Model):
     date_ordered = models.DateTimeField(auto_now_add=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     is_ordered = models.BooleanField(default=False)
+    address = models.CharField(max_length=255, default='')
+    phone_number = models.CharField(max_length=20, default='')
+    full_name = models.CharField(max_length=100, default='')
 
     def __str__(self):
         return f"Order #{self.id} by {self.owner.username}"
