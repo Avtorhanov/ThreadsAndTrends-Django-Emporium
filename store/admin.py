114886ffc187b2ffcf580cedfcdd7e487adfadb0
Ahmad Avtorkhanov <example0612@mail.ru>
2024-02-09 16:12:20 +0300

    Fixed ordering and delivery details

--- store/admin.py
+++ store/admin.py
@@ -29,11 +29,11 @@ class OrderItemInline(admin.TabularInline):
     extra = 0
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['owner', 'total_price', 'is_ordered', 'address', 'phone_number', 'total_quantity', 'status']  # Добавлено поле 'status'
+    list_display = ['owner', 'total_price', 'is_ordered', 'address', 'phone_number', 'total_quantity', 'status']
     search_fields = ['owner__username']
     inlines = [OrderItemInline]
-    fields = ['owner', 'total_price', 'is_ordered', 'address', 'phone_number', 'full_name', 'status']  # Добавлено поле 'status'
-    list_filter = ['status']  # Добавлено поле 'status'
+    fields = ['owner', 'total_price', 'is_ordered', 'address', 'phone_number', 'full_name', 'status']
+    list_filter = ['status']
 
 class OrderItemAdmin(admin.ModelAdmin):
     model = OrderItem
