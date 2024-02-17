0b515d39a40df7d176558f1f0e877197aa5a9dcb
Ahmad Avtorkhanov <example0612@mail.ru>
2024-02-17 11:10:11 +0300

    0.3

--- store/urls.py
+++ store/urls.py
@@ -30,6 +30,8 @@ urlpatterns = [
     path('checkout/<int:item_id>/', views.checkout, name='checkout'),
     path('order/<int:order_id>/', views.order_detail, name='order_detail'),
     path('my-orders/', views.my_orders, name='my_orders'),
+    path('delete_order/<int:order_id>/', views.delete_order, name='delete_order'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
