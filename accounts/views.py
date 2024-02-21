b7b0b648efad616e23473c45ed1db37788773bc1
Ahmad Avtorkhanov <example0612@mail.ru>
2024-02-21 23:45:36 +0300

    added profile, and changing data users

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
-from django.contrib.auth import login as auth_login, logout as auth_logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout, update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import StandardUserCreationForm
+from .forms import StandardUserCreationForm, ProfileUpdateForm
 
 
 def signup(request):
@@ -38,3 +38,36 @@ def log_out_view(request):
     auth_logout(request)
     messages.success(request, 'Вы вышли из магазина.')
     return redirect('home')
+
+
+@login_required
+def profile(request):
+    user = request.user
+
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=user)
+        if form.is_valid():
+            user = form.save()
+            new_password = form.cleaned_data.get('new_password')
+            if new_password:
+                user.set_password(new_password)
+                user.save()
+                update_session_auth_hash(request, user)
+                messages.success(request, 'пароль обновлен.')
+            else:
+                messages.success(request, 'Данные обновлены.')
+            return redirect('profile')
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"Ошибка в поле '{form.fields[field].label}': {error}")
+    else:
+        form = ProfileUpdateForm(instance=user)
+
+    return render(request, 'accounts/profile.html', {'form': form})
+
+
+
+
+
+
